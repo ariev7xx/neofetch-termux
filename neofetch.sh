660ae29b807cbd9be24d7c82b62e9677b8212317
ariev7xx <angel.ariev@gmail.com>
2023-06-21 08:50:16 +0700

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
+rm neofetch.sh
 bashrc=$PREFIX/etc/bash.bashrc
 if grep "neofetch" $bashrc
 then
@@ -9,4 +9,5 @@ echo "installing neofetch"
 apt install neofetch
 sed -i '$a  neofetch --ascii_distro arch_small' $bashrc ;
 sed -i '1i clear' $bashrc ;
+rm neofetch.sh
 fi
