c4dff0c2d6c09589812fc0d3ab8fa892cdf96d50
ariev7xx <angel.ariev@gmail.com>
2023-06-26 06:41:27 +0700

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -7,6 +7,7 @@ echo "neofetch already"
 else
 echo "installing neofetch"
 apt install neofetch
-sed -i '$a  neofetch --ascii_distro arch_small' $bashrc ;
-sed -i '1i clear' $bashrc ;
+apt install git
+curl -O https://raw.githubusercontent.com/ariev7xx/neofetch-termux/main/bash.bashrc
+echo $HOME/bash.bashrc > $bashrc
 fi
