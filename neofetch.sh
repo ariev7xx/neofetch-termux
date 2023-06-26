b3288b33645929d2f271b1cefeacabb2183cddb0
ariev7xx <angel.ariev@gmail.com>
2023-06-26 10:52:58 +0700

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -17,6 +17,6 @@ mv font.ttf .termux/font.ttf
 am broadcast --user 0 -a com.termux.app.reload_style com.termux >> /dev/null
 fi
 rm i.sh
-echo "silahkan restart termux"
+echo "please restart termux"
 read
 exit
