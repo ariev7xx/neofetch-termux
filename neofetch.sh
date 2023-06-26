8da14ffae6777dde443d34795bfe03dfed6c7a74
ariev7xx <angel.ariev@gmail.com>
2023-06-26 08:48:21 +0700

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -13,9 +13,10 @@ apt install perl
 curl -O -s https://raw.githubusercontent.com/ariev7xx/neofetch-termux/main/bash.bashrc
 cat bash.bashrc > $bashrc
 rm bash.bashrc
-fi
 echo "installing font"
 curl -O -s https://raw.githubusercontent.com/ariev7xx/neofetch-termux/main/font.ttf
 mv font.ttf .termux/font.ttf
 am broadcast --user 0 -a com.termux.app.reload_style com.termux >> /dev/null
+fi
+
 source $bashrc
