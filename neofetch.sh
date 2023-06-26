aefacdb3246b314db6f507d8bb2038bd7fef8f7c
ariev7xx <angel.ariev@gmail.com>
2023-06-26 10:12:59 +0700

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -4,14 +4,14 @@ apt update -qqq
 bashrc=$PREFIX/etc/bash.bashrc
 if grep "neofetch" $bashrc &> /dev/null
 then
-echo "neofetch already"
+echo "neofetch already install in bashrc"
 else
-echo "installing neofetch"
+echo "installing neofetch..."
 apt install neofetch git perl -y &> /dev/null
 curl -O -s https://raw.githubusercontent.com/ariev7xx/neofetch-termux/main/bash.bashrc
 cat bash.bashrc > $bashrc
 rm bash.bashrc
-echo "installing font"
+echo "installing font...."
 curl -O -s https://raw.githubusercontent.com/ariev7xx/neofetch-termux/main/font.ttf
 mv font.ttf .termux/font.ttf
 am broadcast --user 0 -a com.termux.app.reload_style com.termux >> /dev/null
