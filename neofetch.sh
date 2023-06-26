23842b9076332c0c066280d6bc32b475db8c3252
ariev7xx <angel.ariev@gmail.com>
2023-06-26 09:06:35 +0700

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -7,9 +7,7 @@ then
 echo "neofetch already"
 else
 echo "installing neofetch"
-apt install neofetch
-apt install git
-apt install perl
+apt install neofetch git perl -y
 curl -O -s https://raw.githubusercontent.com/ariev7xx/neofetch-termux/main/bash.bashrc
 cat bash.bashrc > $bashrc
 rm bash.bashrc
@@ -19,4 +17,4 @@ mv font.ttf .termux/font.ttf
 am broadcast --user 0 -a com.termux.app.reload_style com.termux >> /dev/null
 fi
 echo "restart termux"
-./test.sh
+exac bash
